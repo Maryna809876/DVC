49357f16675d607b3781c15f6b7060c576662628
Марина Арапова <marinchik904@gmail.com>
2024-08-31 22:24:05 +0300

    base structure

---
+++ DVC-test/src/routes/index.jsx
@@ -0,0 +1,16 @@
+import { Routes, Route } from 'react-router-dom';
+
+import EditUserPage from '../pages/EditUserPage/EditUserPage';
+import UserPage from '../pages/UsersPage/UsersPage';
+
+export default () => {
+    return (
+        <Routes>
+            <Route path={'/'} element={<EditUserPage />} />
+            <Route path={'edit'} element={<EditUserPage />} />
+            <Route path={"/user"} element={<UserPage />} />
+
+            <Route path={"*"} element={<div>404 Page is not found</div>} />
+        </Routes>
+    )
+}
